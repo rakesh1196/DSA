bd8fc824c60ccc19ecfcd945a02c53fcee04c5fb
Rakesh Kumar Sahu <98799683+rakesh1196@users.noreply.github.com>
2023-08-14 22:53:33 +0530

    Create usingFunDoubleTheValue.cpp

---
+++ usingFunDoubleTheValue.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int doublekar(int no){
+    int dob = no*2;
+    return dob;
+}
+int main()
+{
+    for (int i=1; i<=5; i++){
+        cout << doublekar(i) << " ";
+    }
+    
+    return 0;
+}
