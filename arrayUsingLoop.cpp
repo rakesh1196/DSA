e216aff867ae621a147583c32486e775e305d5b5
Rakesh Kumar Sahu <98799683+rakesh1196@users.noreply.github.com>
2023-08-14 22:52:00 +0530

    Create arrayUsingLoop.cpp

---
+++ arrayUsingLoop.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int chunu [4];
+    for(int i=0; i<=4; i++){
+        cin >> chunu[i];
+    }
+    for(int i=0; i<=4; i++){
+        cout << chunu[i] << endl;
+    }
+    
+
+    return 0;
+}
