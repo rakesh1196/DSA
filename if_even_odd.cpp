2aa78ccfb9991817f83a2fd2ac0e7955245d0b21
Rakesh Kumar Sahu <98799683+rakesh1196@users.noreply.github.com>
2023-08-23 19:24:04 +0530

    Create if_even_odd.cpp

---
+++ if_even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int h;
+    cout<<"Digit";
+    cin>>h;
+    
+    if (h%2==0){
+        cout<<"even";
+    } else{
+        cout<<"odd";
+    }
+return 0;
+}
