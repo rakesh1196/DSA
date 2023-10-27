0cb3e4f91cb2843e84ebdcbed87b5620fe0b1000
Rakesh Kumar Sahu <98799683+rakesh1196@users.noreply.github.com>
2023-10-27 22:16:54 +0530

    Create patternWithZeros.cpp

---
+++ patternWithZeros.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	int N;
+	cin>>N;
+
+	for (int row=1; row<=N; row++){	
+		for (int col=1; col<=row; col++){
+		if (col == 1 || col==row){
+			cout<<row<<'\t';
+		}else{
+			cout<<"0"<<'\t';	
+		}	
+		}
+			cout<<endl;
+	}
+	return 0;
+}
